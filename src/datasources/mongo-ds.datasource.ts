06cfa5f5fdb232b370b77d814bae0d3eca824243
Abdul Rafay <arafay@nisum.com>
2020-10-23 09:47:20 +0500

    project setup

---
+++ src/datasources/mongo-ds.datasource.ts
@@ -0,0 +1,33 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'mongoDS',
+  connector: 'mongodb',
+  url:
+    'mongodb+srv://MONGO_USER:MONGO_PASSWORD@cluster0.z7how.mongodb.net/loopback?retryWrites=true&w=majority',
+  host: 'cluster0.z7how.mongodb.net',
+  port: 27017,
+  user: 'MONGO_USER',
+  password: 'MONGO_PASSWORD',
+  database: 'loopback',
+  useNewUrlParser: true,
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class MongoDsDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'mongoDS';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.mongoDS', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
